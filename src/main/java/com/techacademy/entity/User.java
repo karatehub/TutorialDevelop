5990cb415de9646c1ad48ae32b2a5553d56d0d4e
naoya.shikatani <chao_gadiop_kun@yahoo.co.jp>
2023-03-01 22:30:56 +0900

    Lesson17 課題

---
+++ src/main/java/com/techacademy/entity/User.java
@@ -0,0 +1,75 @@
+package com.techacademy.entity;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.PreRemove;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+
+    /** 性別用の列挙型 */
+    public static enum Gender {
+        男性, 女性
+    }
+
+    /** 主キー。自動生成 */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /** 名前。20桁。null不許可 */
+    @Column(length = 20, nullable = false)
+    @NotEmpty
+    @Length(max=20)
+    private String name;
+
+    /** 性別。2桁。列挙型（文字列） */
+    @Column(length = 2)
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Gender gender;
+
+    /** 年齢 */
+    @Min(0)
+    @Max(120)
+    private Integer age;
+
+    /** メールアドレス。50桁。null許可 */
+    @Column(length = 50)
+    @Email
+    @Length(max=50)
+    private String email;
+
+    // ----- 追加ここから -----
+    @OneToOne(mappedBy="user")
+    private Authentication authentication;
+
+    /** レコードが削除される前に行う処理 */
+    @PreRemove
+    @Transactional
+    private void preRemove() {
+        // 認証エンティティからuserを切り離す
+        if (authentication!=null) {
+            authentication.setUser(null);
+        }
+    }
+    // ----- 追加ここまで -----
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user(name, gender, age, email)
+    values ("キラメキ太郎", "男性", 27, 'taro.kirameki@email.com');
+insert into user(name, gender, age, email)
+    values ("キラメキ次郎", "男性", 22, 'jiro.kirameki@email.com');
+insert into user(name, gender, age, email)
+    values ("キラメキ花子", "女性", 25, 'hanako.kirameki@email.com');
+INSERT INTO authentication(login_user, password, valid_date, user_id)
+     VALUES ("ktaro", "$2a$08$clh9XaYYznpX9WDqySgiCuUu4znpSeu2oJi5l2Q00UJs42Llrbd7S", "9999-12-31", 1);
